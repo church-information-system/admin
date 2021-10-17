b1e4aaacfe2630d55aff42850897275fe819040d
Jerome <josejeromelalunio@gmail.com>
2021-10-17 17:34:33 +0800

    add search function

--- src/components/content/action-bar.jsx
+++ src/components/content/action-bar.jsx
@@ -1,13 +1,24 @@
 import add from "../../assets/add.svg"
+import Swal from "sweetalert2"
 
-export default function ActionBar() {
+export default function ActionBar({ addPerson, search }) {
     return (
         <div className="action-bar">
             <span className="search-bar">
-                <input type="text" className="search-field" />
-                <div className="action-button" title="Search"><h4>Search</h4></div>
+                <input type="text" className="search-field" id="search-field" />
+                <div className="action-button" title="Search" onClick={
+                    () => search(document.getElementById("search-field").value)
+                }><h4>Search</h4></div>
             </span>
-            <span className="action-button" title="Add">
+            <span className="action-button" title="Add" onClick={() => {
+                Swal.fire({
+                    title: "Enter Name",
+                    html:
+                        '<span class="swal2-input-label">Fullname</span>' +
+                        '<input id="swal-input1" class="swal2-input">',
+                    showCancelButton: true,
+                }).then(() => addPerson(document.getElementById('swal-input1').value))
+            }}>
                 <img src={add} alt="add" className="icon" />
                 <h4>Add Record</h4>
             </span>
--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -2,26 +2,32 @@ import "./content.scss"
 
 import ContentItem from "./content-item"
 import ActionBar from "./action-bar"
+import { useState } from "react"
 
 export default function Content() {
+
+    const [persons, setPersons] = useState(["Juan Dela Cruz"])
+    const [searchString, setSearchString] = useState("");
+
+    const addPerson = (name) => setPersons((current) => [...current, name,])
+    const search = (input) => setSearchString(() => input)
+
+    function getMatches() {
+        return (searchString !== "") ?
+            persons.map((person, index) => {
+                if (person.toLowerCase().includes(searchString.toLowerCase().trim()))
+                    return <ContentItem name={person} key={index} />
+                else return null
+            }) :
+            persons.map((person, index) => <ContentItem name={person} key={index} />)
+    }
+
     return (
         <div id="content">
-            <ActionBar />
-
+            <ActionBar addPerson={addPerson} search={search} />
             <div className="content-container">
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
-                <ContentItem name="Juan Dela Cruz" />
+                {getMatches()[0] ? getMatches() : "No Records found"}
             </div>
-
         </div>
     )
 }
