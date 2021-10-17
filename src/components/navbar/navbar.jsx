96033d2e0f4f4dfd5294c7244c48075d52751cfe
Jerome <josejeromelalunio@gmail.com>
2021-10-17 15:38:03 +0800

    add navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,14 @@
 import "./navbar.scss"
+import settings from "../../assets/settings.svg"
+import Swal from "sweetalert2"
 
 export default function NavBar() {
     return (
-        <div id="navbar">NavBar</div>
+        <div id="navbar">
+            <h2>National Shrine of Our Lady of sorrows</h2>
+            <img src={settings} alt="Settings" className="icon clickable" onClick={() =>
+                Swal.fire({ title: "Settings" })
+            } />
+        </div>
     )
 }
