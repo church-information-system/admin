045a40743c26d670b9211f10be1b5dd530ec7d65
Jerome <josejeromelalunio@gmail.com>
2021-10-20 13:36:15 +0800

    set primary and secondary colors

--- src/components/content/content-item.jsx
+++ src/components/content/content-item.jsx
@@ -13,30 +13,38 @@ export default function ContentItem({ name, address, phone, id, rename, remove }
                 <small>{phone}</small>
             </div>
             <span>
-                <img src={print} title="print" alt="" className="icon clickable" onClick={() => Swal.fire({ title: "print" })} />
-                <img src={edit} title="edit" alt="" className="icon clickable" onClick={() => {
-                    Swal.fire({
-                        title: name,
-                        html:
-                            '<span class="swal2-input-label" >Fullname</span>' +
-                            '<input id="swal-input1" class="swal2-input">',
-                        showCancelButton: true,
-                    }).then(() => {
-                        rename(document.getElementById('swal-input1').value, id)
-                    })
-                }} />
-                <img src={archive} title="archive" alt="archive" className="icon clickable" onClick={() =>
-                    Swal.fire({
-                        title: "Are you sure you want to delete this record?",
-                        icon: "warning",
-                        showCancelButton: true,
-                        confirmButtonText: "archive",
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            remove(id)
-                        }
-                    })
-                } />
+                <div className="icons-container">
+                    <div className="icon-container">
+                        <img src={print} title="print" alt="" className="icon" onClick={() => Swal.fire({ title: "print" })} />
+                    </div>
+                    <div className="icon-container">
+                        <img src={edit} title="edit" alt="" className="icon" onClick={() => {
+                            Swal.fire({
+                                title: name,
+                                html:
+                                    '<span class="swal2-input-label" >Fullname</span>' +
+                                    '<input id="swal-input1" class="swal2-input">',
+                                showCancelButton: true,
+                            }).then(() => {
+                                rename(document.getElementById('swal-input1').value, id)
+                            })
+                        }} />
+                    </div>
+                    <div className="icon-container">
+                        <img src={archive} title="archive" alt="archive" className="icon" onClick={() =>
+                            Swal.fire({
+                                title: "Are you sure you want to delete this record?",
+                                icon: "warning",
+                                showCancelButton: true,
+                                confirmButtonText: "archive",
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    remove(id)
+                                }
+                            })
+                        } />
+                    </div>
+                </div>
             </span>
         </div>
     )
