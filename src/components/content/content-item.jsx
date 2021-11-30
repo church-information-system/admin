a1bf45d5fbba6fadd95b28e8ef2c44f3a34f7c3f
Jerome <josejeromelalunio@gmail.com>
2021-12-01 06:00:14 +0800

    remove upload for schedule

--- src/components/content/content-item.jsx
+++ src/components/content/content-item.jsx
@@ -472,6 +472,7 @@ export default function ContentItem({
           )}
           {selected !== "events" &&
           selected !== "donation" &&
+          selected !== "schedule" &&
           selected !== "requests" ? (
             <div className="icon-container">
               {uploading ? (
