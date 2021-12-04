7cdd2becadde97b3e2c79d3346a289f0763c8edc
Jerome <josejeromelalunio@gmail.com>
2021-12-04 12:56:08 +0800

    add badge:

--- src/components/sidebar/sidebar-item.jsx
+++ src/components/sidebar/sidebar-item.jsx
@@ -1,6 +1,7 @@
 export default function SidebarItem({ imagesrc, label, isSelected }) {
   return (
     <div className={`sidebar-item ${isSelected ? "active" : ""}`}>
+      {label === "Requests" ? <span className="badge">2</span> : ""}
       <img src={imagesrc} alt="icon" className="icon" />
       <div>{label}</div>
     </div>
